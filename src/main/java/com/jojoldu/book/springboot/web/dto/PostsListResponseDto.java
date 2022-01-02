b44f6bd74d43a26a4faebacb0af5449d9f38a695
sumin123 <soomin61@snu.ac.kr>
2022-01-02 15:30:17 +0900

    [4장] mustache로 게시글 CRUD 기능 구현
    mustache로 게시글 화면 및 생성/수정/삭제 기능 구현

--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDto.java
@@ -3,19 +3,19 @@ package com.jojoldu.book.springboot.web.dto;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import lombok.Getter;
 
-@Getter
-public class PostsResponseDto {
+import java.time.LocalDateTime;
 
+@Getter
+public class PostsListResponseDto {
     private Long id;
     private String title;
-    private String content;
     private String author;
+    private LocalDateTime modifiedDate;
 
-    public PostsResponseDto(Posts entity) {
+    public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
-        this.content = entity.getContent();
         this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
     }
-
 }
