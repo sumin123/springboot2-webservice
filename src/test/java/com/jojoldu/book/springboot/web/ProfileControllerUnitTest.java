2eb0037f9e2e5c567403ad4ba1d0a6fb64887c3d
sumin123 <soomin61@snu.ac.kr>
2022-01-16 16:20:13 +0900

    [10장] 무중단 배포 설정

---
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,51 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile() {
+        String expectedProfile = "real1";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void oauth_profile() {
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void default_profile() {
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+}
